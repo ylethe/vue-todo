e73365f62b95b7fbeda6fb18740aa9c3e9fdf770
lethe <1443646652@qq.com>
2017-01-24 14:38:09 +0800

    配置路由

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+/**
+ * Created by lethe on 17-1-24.
+ */
+export default {
+  '/': 'App',
+  '/login': 'Login',
+  '/home': 'Home',
+  '/home/addTodo': 'AddTodo',
+  '/home/TodoList': 'TodoList'
+}
